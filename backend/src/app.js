3082afe8c36d74f09468ec5388a192f033318531
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-09-29 11:40:52 +0530

    feat(auth): add authentication routes for signup and login

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const authRouter = require("./routes/auth.routes");
 const app = express();
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/v1", authRouter);
 
 module.exports = app;
