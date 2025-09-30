e6d11efee60aab5fa6886141c64bb7db43031913
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-09-30 19:20:04 +0530

    feat: added global error handler feat: created AppError as custom Error constructor

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const authRouter = require("./routes/auth.routes");
+const AppError = require("./utils/AppError");
+const { globalErrorHandler } = require("./middlewares/errorHandler");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api/v1", authRouter);
+app.use("/api/v1/auth", authRouter);
+
+// Handle unfound routes (404)
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global error handling middleware
+app.use(globalErrorHandler());
 
 module.exports = app;
